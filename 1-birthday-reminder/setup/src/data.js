17f6d62db10cfdbf47a7cc86b77a5b9636733adf
Chang-Suk <jcs191072@gmail.com>
2021-01-28 14:09:02 +0900

    [ADD] -- Make a Setup For Birthday reminder

---
+++ 1-birthday-reminder/setup/src/data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: 'IU',
+    age: 29,
+    image:
+      'https://i.pinimg.com/564x/75/7c/75/757c755d51a13be47c4d989ff40394b4.jpg',
+  },
+  {
+    id: 2,
+    name: 'Park Seo-jun',
+    age: 32,
+    image:
+      'https://i.pinimg.com/236x/75/4d/3d/754d3d9e114f7b770a4543d47fb028e7.jpg',
+  },
+  {
+    id: 3,
+    name: 'Lee Ju-bin',
+    age: 36,
+    image:
+      'https://i.pinimg.com/236x/88/ba/76/88ba7646c14de14d9b7176a783610a54.jpg',
+  },
+  {
+    id: 4,
+    name: 'Kim sa-rang',
+    age: 34,
+    image:
+      'https://i.pinimg.com/236x/db/ad/cd/dbadcd61f2dd27dbd607a6608a9f7baf.jpg',
+  },
+  {
+    id: 5,
+    name: 'Yoon-ho',
+    age: 29,
+    image:
+      'https://i.pinimg.com/236x/e4/86/62/e486628e664642da837a5127a9be8fef.jpg',
+  },
+];
