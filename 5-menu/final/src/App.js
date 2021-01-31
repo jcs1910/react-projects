d9e1d6c51e8da43a21f0fe5269194f42122d001e
Chang-Suk <jcs191072@gmail.com>
2021-01-31 23:30:10 +0900

    [UPDATE] -- Categories and Menu Componenets rendered

---
+++ 5-menu/final/src/App.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import items from './data';
+
+const itemCategory = new Set(items.map((item) => item.category));
+const itemList = ['all', ...itemCategory];
+
+function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(itemList);
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items);
+      return;
+    }
+
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
+}
+
+export default App;
