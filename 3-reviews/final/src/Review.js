a9a4ee17024e420d971a7e0ff3ffa2ed88a8eb97
Chang-Suk <jcs191072@gmail.com>
2021-01-31 17:32:17 +0900

    [UPDATE] -- Implemented Review logics

--- 3-reviews/final/src/Review.js
+++ 3-reviews/final/src/Review.js
@@ -1,36 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
+import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = people[index];
+
+  const checkNumber = (number) => {
+    if (number > people.length - 1) {
+      return 0;
+    }
+    if (number < 0) {
+      return people.length - 1;
+    }
+    return number;
+  };
+
+  const nextPerson = () => {
+    setIndex((index) => {
+      let newIndex = index + 1;
+      return checkNumber(newIndex);
+    });
+  };
+
+  const prevPerson = () => {
+    setIndex((index) => {
+      let newIndex = index - 1;
+      return checkNumber(newIndex);
+    });
+  };
+
+  const randomPerson = () => {
+    let randomNumber = Math.floor(Math.random() * people.length);
+
+    if (randomNumber === index) {
+      randomNumber = index + 1;
+    }
+    setIndex(checkNumber(randomNumber));
+  };
+
   return (
     <article className="review">
       <div className="img-container">
-        <img
-          src="https://i.pinimg.com/564x/5a/5d/bc/5a5dbcaa85bb335715a6643a8bbf4c3f.jpg"
-          alt=""
-          className="person-img"
-        />
+        <img src={image} alt={name} className="person-img" />
         <span className="quote-icon">
           <FaQuoteRight />
         </span>
       </div>
-      <h4 className="author">In-sun</h4>
-      <p className="job">Actress</p>
-      <p className="info">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo tempora
-        fugiat magnam ut fuga totam unde voluptatibus, minus non quae illo
-        corporis rerum saepe beatae facilis. Veritatis sit quaerat
-        exercitationem.
-      </p>
+      <h4 className="author">{name}</h4>
+      <p className="job">{job}</p>
+      <p className="info">{text}</p>
       <div className="button-container">
-        <button className="prev-btn">
+        <button className="prev-btn" onClick={prevPerson}>
           <FaChevronLeft />
         </button>
-        <button className="next-btn">
+        <button className="next-btn" onClick={nextPerson}>
           <FaChevronRight />
         </button>
       </div>
-      <button className="random-btn">surprise me</button>
+      <button className="random-btn" onClick={randomPerson}>
+        surprise me
+      </button>
     </article>
   );
 };
