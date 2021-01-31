8df485a2a66d680a78d12a41473a9e420bd909c3
Chang-Suk <jcs191072@gmail.com>
2021-01-31 17:31:48 +0900

    [ADD] -- ADD mock data

--- 3-reviews/final/src/data.js
+++ 3-reviews/final/src/data.js
@@ -1,10 +1,10 @@
 const reviews = [
   {
     id: 1,
-    name: 'susan smith',
-    job: 'web developer',
+    name: 'In-sun',
+    job: 'actress',
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+      'https://i.pinimg.com/564x/5a/5d/bc/5a5dbcaa85bb335715a6643a8bbf4c3f.jpg',
     text:
       "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
   },
